a0b3382c6f70169d82458cc438aa9ecca52c2208
erufaku <elfak2013@mail.ru>
2021-10-18 03:23:05 +0300

    git commit -m "last!"

--- test.c
+++
@@ -1,9 +0,0 @@
-#include "ft_split.c"
-#include <stdio.h>
-int main(void)
-{
-	int k = -1;
-	while (++k < 26)
-		printf("%s\n", ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i')[k]);
-	return (0);
-}
